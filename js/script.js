bf936b19dbbc5dead49c2508c475a8a552a8ac43
Aina <ainamoreno@outlook.com>
2022-09-23 19:57:40 +0200

    Iniciación con la lógica

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const btnEncender = document.getElementById("btn-home");
+const pantalla = document.getElementById("")
+
+
+clickBotonEncender = () => {
+    let sound = new Audio('../audio/Bing.mp3');
+    btnEncender.addEventListener('click', () => {
+        sound.play();
+    })
+
+    btnEncender.addEventListener('click', () => {
+        
+    })
+
+}
+
+clickBotonEncender()
