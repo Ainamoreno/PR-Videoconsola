16ec3cbe40b1a5cf557c81d3809cf4b502623f63
Aina <ainamoreno@outlook.com>
2022-09-23 21:06:55 +0200

    He añadido audio y img

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 const btnEncender = document.getElementById("btn-home");
-const pantalla = document.getElementById("")
+const pantalla = document.getElementById("pantalla");
+const videoLoading = document.getElementById("loading");
+const imagenLogo = document.getElementById("img-logo")
 
 
 clickBotonEncender = () => {
     let sound = new Audio('../audio/Bing.mp3');
+    let soundInstro = new Audio('../audio/audio-intro.mp3');
+
+    setTimeout (()=> {
+        btnEncender.addEventListener('click', () => {
+            soundInstro.play()
+            pantalla.classList.add("img-logo");
+        })
+        
+    }, 1000)
+
     btnEncender.addEventListener('click', () => {
         sound.play();
     })
 
     btnEncender.addEventListener('click', () => {
-        
+        btnEncender.classList.add("led-encendido")
+
     })
+
 
 }
 
