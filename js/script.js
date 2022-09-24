fa894f7596d00340af3c380138015e8df91e38e8
Aina <ainamoreno@outlook.com>
2022-09-24 10:58:06 +0200

    Correción de la ejecución del vídeo y sonido

--- js/script.js
+++ js/script.js
@@ -1,30 +1,26 @@
 const btnEncender = document.getElementById("btn-home");
 const pantalla = document.getElementById("pantalla");
 const videoLoading = document.getElementById("loading");
-const imagenLogo = document.getElementById("img-logo")
+const videoIntro = document.getElementById("video");
 
 
 clickBotonEncender = () => {
     let sound = new Audio('../audio/Bing.mp3');
-    let soundInstro = new Audio('../audio/audio-intro.mp3');
 
     setTimeout (()=> {
         btnEncender.addEventListener('click', () => {
-            soundInstro.play()
-            pantalla.classList.add("img-logo");
-        })
-        
-    }, 1000)
+            videoIntro.style.display = 'block';
+            pantalla.classList.add('video-logo');
+            btnEncender.classList.add('led-encendido');
+            sound.play();
+            video.play();
+        });
+    }, 1000);
 
-    btnEncender.addEventListener('click', () => {
-        sound.play();
-    })
-
-    btnEncender.addEventListener('click', () => {
-        btnEncender.classList.add("led-encendido")
-
-    })
-
+    videoIntro.addEventListener('ended', () => {
+        video.style.display = 'none';
+        btnEncender.classList.remove('led-encendido');
+    });
 
 }
 
